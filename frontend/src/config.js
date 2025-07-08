e23b5d8e1afd67c2908bc357206058617d5ab968
Mathew <mathewj@onefin.com>
2025-07-08 21:20:39 +0530

    changed prod base url for frontend

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 
-const API_BASE_URL = import.meta.env.PROD ? 'https://tutorai-prod-6b5900cdee43.herokuapp.com/api' : 'http://localhost:8000/api';
+const API_BASE_URL = import.meta.env.PROD ? 'https://tutor-ai.in/api' : 'http://localhost:8000/api';
 
 export default API_BASE_URL; 
